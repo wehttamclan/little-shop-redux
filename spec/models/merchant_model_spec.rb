23bca9246d5310bba87ed5a052ccbf5da6bec922
rcm32000 <rcm32000@yahoo.com>
2018-06-27 10:31:22 -0600

    Merchant Model Testing for Initial Setup

---
+++ spec/models/merchant_model_spec.rb
@@ -0,0 +1,13 @@
+RSpec.describe Merchant do
+  describe 'Validations' do
+    it 'is invalid without a name' do
+      merchant = Merchant.create(name: nil)
+
+      expect(merchant).to_not be_valid
+    end
+    it 'is invalid with a name' do
+      merchant = Merchant.create(name: 'Test')
+
+      expect(merchant).to_not be_valid
+    end
+  end

0dc9f71dbd4b689eb8417459b5640c8e5b1ac260
rcm32000 <rcm32000@yahoo.com>
2018-06-28 14:52:11 -0600

    adds items table and schema

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,71 @@
+RSpec.describe Item do
+  describe 'Validations' do
+    it 'is invalid without a title' do
+      item = Item.create(title: nil,
+                         description: 'something',
+                         price: 200,
+                         image: 'www.example.com')
+
+      expect(item).to_not be_valid
+    end
+    it 'is valid with a title' do
+      item = Item.create(title: "some item",
+                         description: 'something',
+                         price: 200,
+                         image: 'www.example.com')
+
+      expect(item).to be_valid
+    end
+
+    it 'is invalid without a description' do
+      item = Item.create(title: 'some item',
+                         description: nil,
+                         price: 200,
+                         image: 'www.example.com')
+
+      expect(item).to_not be_valid
+    end
+    it 'is valid with a description' do
+      item = Item.create(title: 'some item',
+                         description: 'something',
+                         price: 200,
+                         image: 'www.example.com')
+
+      expect(item).to be_valid
+    end
+
+    it 'is invalid without a price' do
+      item = Item.create(title: 'some item',
+                         description: 'something',
+                         price: nil,
+                         image: 'www.example.com')
+
+      expect(item).to_not be_valid
+    end
+    it 'is valid with a price' do
+      item = Item.create(title: 'some item',
+                         description: 'something',
+                         price: 200,
+                         image: 'www.example.com')
+
+      expect(item).to be_valid
+    end
+
+    it 'is invalid without a image' do
+      item = Item.create(title: 'some item',
+                         description: 'something',
+                         price: 200,
+                         image: nil)
+
+      expect(item).to_not be_valid
+    end
+    it 'is valid with a image' do
+      item = Item.create(title: 'some item',
+                         description: 'something',
+                         price: 200,
+                         image: 'www.example.com')
+
+      expect(item).to be_valid
+    end
+  end
+end

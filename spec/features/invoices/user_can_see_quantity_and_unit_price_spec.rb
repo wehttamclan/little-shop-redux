37706dc277c1d2e5a979eedaf127ebce2be914d7
Matthew Peters <JiveTurkey@MattBook-Pro.local>
2018-07-01 11:13:15 -0600

    adds total_price method for invoice into the show page

--- spec/features/invoices/user_can_see_quantity_and_unit_price_spec.rb
+++ spec/features/invoices/user_can_see_quantity_and_unit_price_spec.rb
@@ -33,5 +33,26 @@ describe 'User' do
 
       expect(page).to have_content(expected_unit_price)
     end
+
+    it 'should see total_price of invoice' do
+      test_invoice = Invoice.create(merchant_id: 1,
+                                    status: 'pending')
+
+      invoice_item_1 = InvoiceItem.create(item_id: 24355,
+                                             invoice_id: 1,
+                                             quantity: 1,
+                                             unit_price: 100)
+
+      invoice_item_2 = InvoiceItem.create(item_id: 23,
+                                             invoice_id: 1,
+                                             quantity: 3,
+                                             unit_price: 200)
+
+      expected = test_invoice.total_price
+
+      visit("/invoices/#{test_invoice.id}")
+
+      expect(page).to have_content(expected)
+    end
   end
 end

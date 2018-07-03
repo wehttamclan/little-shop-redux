4fb4ad727c81b90408baa7cc72af2a072af8830d
rcm32000 <rcm32000@yahoo.com>
2018-07-03 12:32:51 -0600

    Invoice Edit page tested

--- app/controllers/little_shop_app.rb
+++ app/controllers/little_shop_app.rb
@@ -109,7 +109,6 @@ class LittleShopApp < Sinatra::Base
 
   get '/invoices/:id/edit' do
     @invoice = Invoice.find(params[:id])
-    # binding.pry
     erb :'invoices/edit'
   end
 
--- spec/features/invoices/invoice_page_links_and_buttons_spec.rb
+++ spec/features/invoices/invoice_page_links_and_buttons_spec.rb
@@ -1,29 +1,49 @@
 RSpec.describe do
-  context 'dashboard page' do
-    before(:each) do
-    merchant = Merchant.create(name: 'Whatever')
-    invoice1 = Invoice.create(merchant_id: 1,
-                              status: 'pending')
-    invoice2 = Invoice.create(merchant_id: 1,
-                              status: 'pending')
-    invoice3 = Invoice.create(merchant_id: 1,
-                              status: 'pending')
-    invoice4 = Invoice.create(merchant_id: 1,
-                              status: 'pending')
-    invoice_item1 = invoice1.invoice_items.create(item_id: 10,
-                                                  quantity: 3,
-                                                  unit_price: 100)
-    invoice_item2 = invoice2.invoice_items.create(item_id: 20,
-                                                  quantity: 3,
-                                                  unit_price: 10)
-    invoice_item3 = invoice3.invoice_items.create(item_id: 30,
-                                                  quantity: 10,
-                                                  unit_price: 44)
-    invoice_item4 = invoice4.invoice_items.create(item_id: 40,
-                                                  quantity: 1,
-                                                  unit_price: 44)
+  before(:each) do
+    @merchant = Merchant.create(name: 'Whatever')
+    @item1 = Item.create(id: 10,
+                         title: 'Item One',
+                         description: 'description',
+                         price: 100,
+                         image: 'not found')
+    @item2 = Item.create(id: 20,
+                         title: 'Item One',
+                         description: 'description',
+                         price: 10,
+                         image: 'not found')
+    @item3 = Item.create(id: 30,
+                         title: 'Item One',
+                         description: 'description',
+                         price: 44,
+                         image: 'not found')
+    @item4 = Item.create(id: 40,
+                         title: 'Item One',
+                         description: 'description',
+                         price: 44,
+                         image: 'not found')
+    @invoice1 = Invoice.create(merchant_id: 1,
+                               status: 'pending')
+    @invoice2 = Invoice.create(merchant_id: 1,
+                               status: 'pending')
+    @invoice3 = Invoice.create(merchant_id: 1,
+                               status: 'pending')
+    @invoice4 = Invoice.create(merchant_id: 1,
+                               status: 'pending')
+    @invoice_item1 = @invoice1.invoice_items.create(item_id: 10,
+                                                    quantity: 3,
+                                                    unit_price: 100)
+    @invoice_item2 = @invoice2.invoice_items.create(item_id: 20,
+                                                    quantity: 3,
+                                                    unit_price: 10)
+    @invoice_item3 = @invoice3.invoice_items.create(item_id: 30,
+                                                    quantity: 10,
+                                                    unit_price: 44)
+    @invoice_item4 = @invoice4.invoice_items.create(item_id: 40,
+                                                    quantity: 1,
+                                                    unit_price: 44)
     @invoice_items = InvoiceItem.all
-    end
+  end
+  context 'dashboard page' do
     it 'can link to invoice with highest unit price' do
       visit '/invoices-dashboard'
 
@@ -47,6 +67,14 @@ RSpec.describe do
 
       click_link('4')
       expect(current_path).to eq("/invoices/#{@invoice_items.lowest_quantity}")
+    end
+  end
+  context 'edit page' do
+    it 'can cancel the update' do
+      visit "/invoices/#{@invoice1.id}/edit"
+      click_link('Cancel')
+
+      expect(current_path).to eq('/invoices/1')
     end
   end
 end
